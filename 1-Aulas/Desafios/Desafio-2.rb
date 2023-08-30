dd87731744fa03ff9a5fd178d1b57849086028d5
RonaldoAraujo2023 <143138115+RonaldoAraujo2023@users.noreply.github.com>
2023-08-30 09:18:30 -0300

    Segundo Desadio

---
+++ 1-Aulas/Desafios/Desafio-2.rb
@@ -0,0 +1,13 @@
+
+print "Entre com o primeiro valor "
+
+firstValue  = gets.to_i
+
+print "Entre com o segundo valor "
+
+secondValue  = gets.to_i
+
+PROD = firstValue * secondValue
+
+puts "O calculo do produto destes valores são #{PROD} "
+
